929cf8763e83bb26243e5410897f17a32e27d948
Hoang Vu Hung <hoanghung250604@gmail.com>
2023-12-28 23:53:34 +0700

    Implement query command with Parameters.AddWithValue()

--- Models/DAO/AttendanceDAO.cs
+++ Models/DAO/AttendanceDAO.cs
@@ -7,76 +7,93 @@ namespace AttendanceApplication.Models.DAO
     {
         private const string connString = "Data Source=localhost; Initial Catalog=AttendanceApplication; User ID=SA; Password=2562004; Integrated Security=True";
 
-        public AttendanceDAO() { }  
+        public AttendanceDAO() { }
 
-        public bool InsertCheckInStatus(string username, string curentTime, string curentDate, string position)
-        {
-            using (SqlConnection dbConnection = new SqlConnection(connString))
-            {
-                dbConnection.Open();   
-                DbTransaction transaction = dbConnection.BeginTransaction();
-                try
-                {
-                    string commandText = "INSERT INTO [AttendanceApplication].[dbo].[Attendance] (username, start_time, attendance_date, position) " +
-                        "VALUES ('" + username + "','" + curentTime + "', '" + curentDate + "', '" + position + "')";  
-                    SqlCommand command = new SqlCommand(commandText, transaction.Connection as SqlConnection, (SqlTransaction)transaction);
-                    command.CommandTimeout = 30;
-
-                    command.ExecuteNonQuery(); // Execute the command
-
-                    // if insert, update, delete use Commit()
-                    transaction.Commit();
-                    return true;
-                }
-                catch (Exception ex)
-                {
-                    // if exception occur => use rollback to prevent data lossing
-                    transaction.Rollback();
-                    Console.WriteLine(ex.StackTrace);
-                    Console.WriteLine(ex.Message);
-                    return false;
-                    throw;
-                }
-                finally
-                {
-                    dbConnection.Close();
-                }
-            }
-        }
-
-        public bool UpdateCheckOutStatus(string username, string curentTime)
+        public AttendanceStatus InsertCheckInStatus(string username, string startTime, string currentDate, string position)
         {
             using (SqlConnection dbConnection = new SqlConnection(connString))
             {
                 dbConnection.Open();
-                DbTransaction transaction = dbConnection.BeginTransaction();
-                try
+                using (DbTransaction transaction = dbConnection.BeginTransaction())
                 {
-                    string commandText = "UPDATE [AttendanceApplication].[dbo].[Attendance] " +
-                        "SET finish_time='" + curentTime + "' " +
-                        "WHERE username='" + username + "' AND finish_time IS NULL";
-                    SqlCommand command = new SqlCommand(commandText, transaction.Connection as SqlConnection, (SqlTransaction)transaction);
-                    command.CommandTimeout = 30;
+                    try
+                    {
+                        string query = "INSERT INTO [AttendanceApplication].[dbo].[Attendance] (username, start_time, attendance_date, position) " +
+                         "VALUES (@Username, @StartTime, @CurrentDate, @Position)";
 
-                    command.ExecuteNonQuery(); // Execute the command
+                        using (SqlCommand command = new SqlCommand(query, dbConnection))
+                        {
+                            command.Transaction = (SqlTransaction)transaction;
+                            command.Parameters.AddWithValue("@Username", username);
+                            command.Parameters.AddWithValue("@StartTime", startTime);
+                            command.Parameters.AddWithValue("@CurrentDate", currentDate);
+                            command.Parameters.AddWithValue("@Position", position);
 
-                    // if insert, update, delete use Commit()
-                    transaction.Commit();
-                    return true;
+                            command.ExecuteNonQuery();
+                            transaction.Commit();
+                        }
+
+                        // if insert, update, delete use Commit()
+                        return new AttendanceStatus(username, TimeOnly.Parse(startTime), DateOnly.Parse(currentDate), position);
+                    }
+                    catch (Exception ex)
+                    {
+                        // if exception occur => use rollback to prevent data lossing
+                        transaction.Rollback();
+                        Console.WriteLine(ex.StackTrace);
+                        Console.WriteLine(ex.Message);
+                        throw;
+                    }
+                    finally
+                    {
+                        dbConnection.Close();
+                    }
                 }
-                catch (Exception ex)
+            }
+        }
+
+        public bool UpdateCheckOutStatus(string username, string finishTime)
+        {
+            using (SqlConnection dbConnection = new SqlConnection(connString))
+            {
+                dbConnection.Open();
+                using (DbTransaction transaction = dbConnection.BeginTransaction())
                 {
-                    // if exception occur => use rollback to prevent data lossing
-                    transaction.Rollback();
-                    Console.WriteLine(ex.StackTrace);
-                    Console.WriteLine(ex.Message);
-                    return false;
-                    throw;
+                    try
+                    {
+                        string query = "UPDATE [AttendanceApplication].[dbo].[Attendance] " +
+                            "SET finish_time=@FinishTime " +
+                            "WHERE username=@Username AND finish_time IS NULL";
+
+                        using (SqlCommand command = new SqlCommand(query, dbConnection))
+                        {
+                            command.Transaction = (SqlTransaction)transaction;
+                            command.Parameters.AddWithValue("@Username", username);
+                            command.Parameters.AddWithValue("@FinishTime", finishTime);
+
+                            command.CommandTimeout = 30;
+                            command.ExecuteNonQuery();
+                        }
+
+                        // if insert, update, delete use Commit()
+                        transaction.Commit();
+                        return true;
+                    }
+                    catch (Exception ex)
+                    {
+                        // if exception occur => use rollback to prevent data lossing
+                        transaction.Rollback();
+                        Console.WriteLine(ex.StackTrace);
+                        Console.WriteLine(ex.Message);
+                        return false;
+                        throw;
+                    }
+                    finally
+                    {
+                        dbConnection.Close();
+                    }
                 }
-                finally
-                {
-                    dbConnection.Close();
-                }
+
             }
         }
     }
